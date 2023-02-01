2a24f8154bec648464a8ad87e5e2f684bacba0f3
adeninque <adilmarsov5@gamil.com>
2023-02-01 10:23:14 +0600

    created project function

--- m/views.py
+++ m/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpRequest
 
 # Create your views here.
 def home(request):
   return HttpResponse('Hello from home function')
+
+def project(request: HttpRequest, pk: int):
+  return HttpResponse(f'Project wiht pk: {pk}')
