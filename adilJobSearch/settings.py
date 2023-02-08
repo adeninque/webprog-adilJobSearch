c10c34322426502b2a78493085d6013115b59ebe
adeninque <adilmarsov5@gamil.com>
2023-02-08 10:46:23 +0600

    08-02-2023 lesson

--- adilJobSearch/settings.py
+++ adilJobSearch/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'adilJobSearch.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'eflfmqqb', 
+        'USER': 'eflfmqqb',
+        'PASSWORD': 'AEyz-WUYFJfEzkMeYxAlKnoOgwyIUb-1',
+        'HOST': 'snuffleupagus.db.elephantsql.com', 
+        'PORT': '5432',
     }
 }
 
--- m/admin.py
+++ m/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+import datetime
+
+from .models import *
 
 # Register your models here.
+class ProjectAdmin(admin.ModelAdmin):
+  list_display = ('id', 'slug', 'title', 'created')
+  list_display_links = ('id', 'slug',)
+  prepopulated_fields = {'slug': ('title',)}
+  
+admin.site.register(Project, ProjectAdmin)
